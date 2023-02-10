dd05fc2c2dba0340bdfaae0eb023e9cfbce7887c
zxy <zxy@123456>
2023-02-10 12:10:08 +0800

    hot-fix first commit

--- src/main/java/khv.java
+++ src/main/java/khv.java
@@ -2,6 +2,8 @@ public class khv {
     public static void main(String[] args) {
         System.out.println("dqwedwq");
         System.out.println("dqwedwq");
+        System.out.println("dqwedwq");
+        System.out.println("dqwedwq");
 
     }
 }
